a09e3230706513e9a7920029c73566877349ac2b
hfp <hefeiping.love@163.com>
2018-12-29 15:58:16 +0800

    add download

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var path = require('path')
 var server = http.createServer(function(request, response){
     var pathObj = url.parse(request.url, true);                         // 获取输入的url解析后的对象
     var staticPath = path.resolve(__dirname, 'static');                 // static文件夹的绝对路径
-    var filePath = path.join(staticPath, decodeURI(pathObj.pathname));  // 获取资源文件绝对路径
+    var pathname = decodeURI(pathObj.pathname);
+    var filePath = path.join(staticPath, pathname);                     // 获取资源文件绝对路径
+
     fs.readFile(filePath, 'binary', function(err, fileContent){         // 异步读取file
         if(err){
             console.log(filePath+'  404')
@@ -14,7 +16,14 @@ var server = http.createServer(function(request, response){
             response.end('<h1>404 Not Found</h1>')
         }else{
             console.log(filePath+'  ok')
-            if( filePath.match(/\.css$/)){
+
+            if(pathname.match(/^\/download\//)){
+                var filename = pathname.match(/.+\/(.+)$/)[1];
+                response.writeHead(200, {
+                    'Content-Type':'multipart/form-data',
+                    'Content-Disposition':'attachment;fileName='+filename
+                });
+            }else if( pathname.match(/\.css$/)){
                 response.writeHead(200, {'Content-Type':'text/css'});
             }else{
                 response.writeHead(200, {'Content-Type':'text/html'});
