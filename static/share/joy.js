87d37f78d8ddf385e51d513cd035256cfef87fc0
hfp <hefeiping.love@163.com>
2019-03-18 09:51:10 +0800

    mod

---
+++ static/share/joy.js
@@ -0,0 +1,11 @@
+
+
+$("#detailClick").click(function(){
+    $("#screen").show();
+    $("#detail").show();
+});
+$("#detailClose").click(function(){
+    $("#screen").hide();
+    $("#detail").hide();
+});
+
